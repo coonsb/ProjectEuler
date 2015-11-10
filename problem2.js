0357efd9eebb1eccdfc17cc92571debd6e2aa5a4
Ben Coons <coonsb@purdue.edu>
2015-11-10 01:08:02 -0500

    Finished problem 2

---
+++ problem2.js
@@ -0,0 +1,15 @@
+var total = 0;
+var first = 1;
+var second = 2;
+var third = 0;
+
+while (first < 4000000) {
+    if (first % 2 === 0) {
+        total += first;
+    }
+    third = first + second;
+    first = second;
+    second = third;
+}
+
+console.log(total);
